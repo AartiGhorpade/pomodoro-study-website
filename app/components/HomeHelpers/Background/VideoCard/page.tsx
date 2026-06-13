445f5d62f547ad970ad26cd0fe0514cdf0db5635
AartiGhorpade <asghorpade99@gmail.com>
2026-06-13 13:09:24 +0530

    bug resolved

--- app/components/HomeHelpers/Background/VideoCard/page.tsx
+++ app/components/HomeHelpers/Background/VideoCard/page.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+// import React from "react";
 
-const VideoCard = React.memo(
-  ({ space, setBg }: { space: any; setBg: (url: string) => void }) => {
-    return (
-      <div
-        className="group cursor-pointer overflow-hidden rounded-lg"
-        onClick={() => setBg(space.video)}
-      >
-        <video
-          src={space.video}
-          muted
-          preload="none"
-          className="w-full h-24 object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
-        />
-      </div>
-    );
-  },
-);
+// const VideoCard = React.memo(
+//   ({ space, setBg }: { space: any; setBg: (url: string) => void }) => {
+//     return (
+//       <div
+//         className="group cursor-pointer overflow-hidden rounded-lg"
+//         onClick={() => setBg(space.video)}
+//       >
+//         <video
+//           src={space.video}
+//           muted
+//           preload="none"
+//           className="w-full h-24 object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
+//         />
+//       </div>
+//     );
+//   },
+// );
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
